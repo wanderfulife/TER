f173c0b75317c1f39ba13f61def793a287a8b1b5
J Wander <jowander@student.42.fr>
2024-11-12 14:46:04 +0100

    filtre

--- src/components/surveyQuestions.js
+++ src/components/surveyQuestions.js
@@ -1,3 +1,173 @@
+export const trainData = {
+	1: { // "Brest - Landerneau - Morlaix"
+		name: "Brest - Landerneau - Morlaix",
+		trains: [
+			{ id: "855781", text: "855781 - LANNION/BREST (6:00-7:35)" },
+			{ id: "855705", text: "855705 - MORLAIX/BREST (06:01-06:49)" },
+			{ id: "855053", text: "855053 - LANDERNEAU/BREST (6:55-7:11)" },
+			{ id: "855704", text: "855704 - BREST/MORLAIX (06:57-7:44)" },
+			{ id: "855054", text: "855054 - BREST/LANDERNEAU (7:27-7:44)" },
+			{ id: "855055", text: "855055 - LANDERNEAU/BREST (7:55-8:11)" },
+			{ id: "855711", text: "855711 - MORLAIX/BREST (07:55-08:50)" },
+			{ id: "855710", text: "855710 - BREST/MORLAIX (08:04-08:47)" },
+			{ id: "855056", text: "855056 - BREST/LANDERNEAU (08:24-8:44)" },
+			{ id: "855065", text: "855065 - LANDERNEAU/BREST (12:00-12:16)" },
+			{ id: "855721", text: "855721 - MORLAIX/BREST (12:07-12:56)" },
+			{ id: "855068", text: "855068 - BREST/LANDERNEAU (12:27-12:44)" },
+			{ id: "855720", text: "855720 - BREST/MORLAIX (12:48-13:31)" },
+			{ id: "855067", text: "855067 - LANDERNEAU/BREST (12:55-13:11)" },
+			{ id: "855727", text: "855727 - MORLAIX/BREST (13:03-13:52)" },
+			{ id: "855730", text: "855730 - BREST/MORLAIX (15:54-16:40)" },
+			{ id: "855078", text: "855078 - BREST/LANDERNEAU (16:30-16:47)" },
+			{ id: "855736", text: "855736 - BREST/MORLAIX (16:56-17:45)" },
+			{ id: "855745", text: "855745 - MORLAIX/BREST (17:08-17:53)" },
+			{ id: "855080", text: "855080 - BREST/LANDERNEAU (17:23-17:44)" },
+			{ id: "855742", text: "855742 - BREST/MORLAIX (17:46-18:36)" },
+			{ id: "855079", text: "855079 - LANDERNEAU/BREST (17:55-18:11)" },
+			{ id: "855749", text: "855749 - MORLAIX/BREST (18:10-18:53)" },
+			{ id: "855082", text: "855082 - BREST/LANDERNEAU (18:27-18:44)" },
+			{ id: "855089", text: "855089 - LANDERNEAU/BREST (18:55-19:15)" },
+			{ id: "855752", text: "855752 - BREST/MORLAIX (19:01-19:44)" },
+			{ id: "855092", text: "855092 - BREST/LANDERNEAU (20:27-20:44)" }
+		]
+	},
+	2: { // "Brest - Quimper"
+		name: "Brest - Quimper",
+		trains: [
+			{ id: "854805", text: "854805 - QUIMPER/BREST (6:01-07:28)" },
+			{ id: "854808", text: "854808 - BREST/QUIMPER (06:33-7:55)" },
+			{ id: "854812", text: "854812 - BREST/QUIMPER (7:41-9:00)" },
+			{ id: "854823", text: "854823 - QUIMPER/BREST (8:05-09:30)" },
+			{ id: "854818", text: "854818 - BREST/QUIMPER (8:37-9:54)" },
+			{ id: "854827", text: "854827 - QUIMPER/BREST (9:10-10:19)" },
+			{ id: "854835", text: "854835 - QUIMPER/BREST (10:30-11:46)" },
+			{ id: "854826", text: "854826 - BREST/QUIMPER (10:53-12:09)" },
+			{ id: "854830", text: "854830 - BREST/QUIMPER (11:56-13:09)" },
+			{ id: "854841", text: "854841 - QUIMPER/BREST (12:09-13:26)" },
+			{ id: "854845", text: "854845 - QUIMPER/BREST (12:19-13:31)" },
+			{ id: "854836", text: "854836 - BREST/QUIMPER (12:37-13:56)" },
+			{ id: "854849", text: "854849 - QUIMPER/BREST (13:11-14:31)" },
+			{ id: "854840", text: "854840 - BREST/QUIMPER (13:33-14:52)" },
+			{ id: "854844", text: "854844 - BREST/QUIMPER (13:37-14:53)" },
+			{ id: "854853", text: "854853 - QUIMPER/BREST (14:08-15:23)" },
+			{ id: "854846", text: "854846 - BREST/QUIMPER (14:21-15:49)" },
+			{ id: "854857", text: "854857 - QUIMPER/BREST (15:05-16:14)" },
+			{ id: "854848", text: "854848 - BREST/QUIMPER (15:28-16:44)" },
+			{ id: "854863", text: "854863 - QUIMPER/BREST (16:00-17:12)" },
+			{ id: "854858", text: "854858 - BREST/QUIMPER (16:25-17:45)" },
+			{ id: "854877", text: "854877 - QUIMPER/BREST (17:01-18:25)" },
+			{ id: "854867", text: "854867 - QUIMPER/BREST (17:01-18:17)" },
+			{ id: "854862", text: "854862 - BREST/QUIMPER (17:32-18:53)" },
+			{ id: "854866", text: "854866 - BREST/QUIMPER (17:52-19:02)" },
+			{ id: "854872", text: "854872 - BREST/QUIMPER (17:57-19:10)" },
+			{ id: "854881", text: "854881 - QUIMPER/BREST (18:09-19:32)" },
+			{ id: "854876", text: "854876 - BREST/QUIMPER (18:36-19:58)" },
+			{ id: "854887", text: "854887 - QUIMPER/BREST (19:14-20:34)" },
+			{ id: "854882", text: "854882 - BREST/QUIMPER (19:39-21:05)" },
+			{ id: "854886", text: "854886 - BREST/QUIMPER (19:59-21:15)" },
+			{ id: "854891", text: "854891 - QUIMPER/BREST (21:34-22:50)" }
+		]
+	},
+	3: { // "Quimper - Nantes"
+		name: "Quimper - Nantes",
+		trains: [
+			{ id: "858258", text: "858258 - QUIMPER/NANTES (5:35-8:16)" },
+			{ id: "858286", text: "858286 - BREST/NANTES (7:50-11:05)" },
+			{ id: "858268", text: "858268 - QUIMPER/NANTES (12:02-14:46)" },
+			{ id: "858260", text: "858260 - QUIMPER/NANTES (12:36-15:04)" },
+			{ id: "858261", text: "858261 - NANTES/QUIMPER (12:42-14:53)" },
+			{ id: "858262", text: "858262 - QUIMPER/NANTES (13:35-16:03)" },
+			{ id: "858250", text: "858250 - QUIMPER/NANTES (15:03-17:33)" },
+			{ id: "858278", text: "858278 - QUIMPER/NANTES (17:55-20:35)" },
+			{ id: "858279", text: "858279 - NANTES/QUIMPER (18:27-20:57)" },
+			{ id: "858276", text: "858276 - QUIMPER/NANTES (18:35-21:16)" }
+		]
+	},
+	4: { // "Rennes - Brest"
+		name: "Rennes - Brest",
+		trains: [
+			{ id: "855830", text: "855830 - BREST/RENNES (13:03-15:25)" },
+			{ id: "855831", text: "855831 - RENNES/BREST (14:35-16:58)" },
+			{ id: "855857", text: "855857 - RENNES/BREST (17:00-19:21)" },
+			{ id: "855870", text: "855870 - BREST/RENNES (19:03-21:25)" },
+			{ id: "855886", text: "855886 - BREST/RENNES (20:04-22:24)" }
+		]
+	},
+	5: { // "Rennes - Châteaubriant"
+		name: "Rennes - Châteaubriant",
+		trains: [
+			{ id: "854007", text: "854007 - CHÂTEAUBRIANT/RENNES (06:05-7:15)" },
+			{ id: "854017", text: "854017 - CHÂTEAUBRIANT/RENNES (06:45-7:55)" },
+			{ id: "854027", text: "854027 - CHÂTEAUBRIANT/RENNES (07:25-8:35)" },
+			{ id: "854008", text: "854008 - RENNES/CHÂTEAUBRIANT (07:27-8:49)" },
+			{ id: "854031", text: "854031 - CHÂTEAUBRIANT/RENNES (08:15-9:25)" },
+			{ id: "854037", text: "854037 - CHÂTEAUBRIANT/RENNES (09:05-10:15)" },
+			{ id: "854012", text: "854012 - RENNES/CHÂTEAUBRIANT (10:40-11:50)" },
+			{ id: "854018", text: "854018 - RENNES/CHÂTEAUBRIANT (12:55-14:05)" },
+			{ id: "854041", text: "854041 - CHÂTEAUBRIANT/RENNES (13:15-14:25)" },
+			{ id: "854047", text: "854047 - CHÂTEAUBRIANT/RENNES (14:15-15:25)" },
+			{ id: "854022", text: "854022 - RENNES/CHÂTEAUBRIANT (14:35-15:45)" },
+			{ id: "854028", text: "854028 - RENNES/RETIERS (16:15-16:56)" },
+			{ id: "854038", text: "854038 - RENNES/RETIERS (16:55-17:36)" },
+			{ id: "854057", text: "854057 - RETIERS/RENNES (17:10-18:03)" },
+			{ id: "854051", text: "854051 - CHÂTEAUBRIANT/RENNES (17:14-18:25)" },
+			{ id: "854048", text: "854048 - RENNES/CHÂTEAUBRIANT (17:35-18:46)" },
+			{ id: "854058", text: "854058 - RENNES/CHÂTEAUBRIANT (18:15-19:25)" },
+			{ id: "854067", text: "854067 - RETIERS/RENNES (18:30-19:17)" },
+			{ id: "854042", text: "854042 - RENNES/CHÂTEAUBRIANT (18:35-19:45)" },
+			{ id: "854068", text: "854068 - RENNES/CHÂTEAUBRIANT (19:39-20:49)" }
+		]
+	},
+	11: { // "Rennes - Quimper"
+		name: "Rennes - Quimper",
+		trains: [
+			{ id: "856608", text: "856608 - QUIMPER/RENNES (5:35-7:56)" },
+			{ id: "856614", text: "856614 - QUIMPER/RENNES (10:38-12:56)" },
+			{ id: "856625", text: "856625 - RENNES/QUIMPER (11:35-13:59)" },
+			{ id: "856620", text: "856620 - QUIMPER/RENNES (12:02-14:25)" },
+			{ id: "856624", text: "856624 - QUIMPER/RENNES (12:06-14:25)" },
+			{ id: "856641", text: "856641 - RENNES/QUIMPER (13:35-15:59)" },
+			{ id: "856630", text: "856630 - QUIMPER/RENNES (14:07-16:25)" },
+			{ id: "856634", text: "856634 - QUIMPER/RENNES (14:07-16:21)" },
+			{ id: "856649", text: "856649 - RENNES/QUIMPER (14:35-16:50)" },
+			{ id: "856640", text: "856640 - QUIMPER/RENNES (15:57-18:17)" },
+			{ id: "856655", text: "856655 - RENNES/QUIMPER (16:05-18:25)" },
+			{ id: "856644", text: "856644 - QUIMPER/RENNES (16:11-18:25)" },
+			{ id: "856657", text: "856657 - RENNES/QUIMPER (16:35-18:53)" },
+			{ id: "856659", text: "856659 - RENNES/QUIMPER (17:05-19:29)" },
+			{ id: "856650", text: "856650 - QUIMPER/RENNES (17:16-19:28)" },
+			{ id: "856667", text: "856667 - RENNES/QUIMPER (17:35-19:50)" },
+			{ id: "856656", text: "856656 - QUIMPER/RENNES (17:55-20:17)" },
+			{ id: "856660", text: "856660 - QUIMPER/RENNES (18:35-20:56)" },
+			{ id: "856677", text: "856677 - RENNES/QUIMPER (18:40-20:55)" },
+			{ id: "856670", text: "856670 - QUIMPER/RENNES (19:44-22:03)" },
+			{ id: "856676", text: "856676 - QUIMPER/RENNES (20:06-22:17)" }
+		]
+	},
+	14: { // "Rennes - Vannes"
+		name: "Rennes - Vannes",
+		trains: [
+			{ id: "856141", text: "856141 - RENNES/VANNES (15:35-16:50)" }
+		]
+	},
+	15: { // "Rennes - Vitré"
+		name: "Rennes - Vitré",
+		trains: [
+			{ id: "855276", text: "855276 - RENNES/VITRE (19:02-19:37)" }
+		]
+	},
+	16: { // "St Brieuc - Lannion" (mapped from "St Brieuc - Dol" in the document)
+		name: "St Brieuc - Dol",
+		trains: [
+			{ id: "854104", text: "854104 - DINAN/DOL DE BRETAGNE (5:58-6:18)" },
+			{ id: "854160", text: "854160 - SAINT BRIEUC/DINAN (06:45-07:33)" },
+			{ id: "854194", text: "854194 - DINAN/RENNES (7:01-8:03)" },
+			{ id: "854163", text: "854163 - DINAN/SAINT BRIEUC (7:41-8:31)" },
+			{ id: "854197", text: "854197 - RENNES/DINAN (17:25-18:25)" }
+		]
+	}
+};
+
 export const questions = [
 	{
 		id: "Q1",
@@ -25,9 +195,23 @@ export const questions = [
 	{
 		id: "Q2",
 		text: "Numéro de train",
-		freeText: true,
-		next: "Q3",
-	},
+		options: (answers) => {
+		  // Get the selected axis ID from Q1
+		  const selectedAxisId = answers.Q1;
+		  
+		  // If no axis is selected or the axis doesn't exist in our data, return empty array
+		  if (!selectedAxisId || !trainData[selectedAxisId]) {
+			return [];
+		  }
+		  
+		  // Return the trains for the selected axis
+		  return trainData[selectedAxisId].trains.map(train => ({
+			id: train.id,
+			text: train.text,
+			next: "Q3"
+		  }));
+		},
+	  },
 	{
 		id: "Q3",
 		text: (answers) => "Gare d'arrêt",
